55df8e5db1daf5e283345a7fed3f6b192764c7ec
Smiti Kothari <kotharismiti24@gmail.com>
2025-07-06 04:55:26 +0530

    Update u2net_load.py

--- u2net_load.py
+++ u2net_load.py
@@ -7,7 +7,7 @@ from u2net.model import U2NETP # small version u2net 4.7 MB
 def model(model_name='u2net'):
 
 
-    model_dir = '/content/AI_Virtual_Wardrobe/u2net/saved_models/u2netp/u2netp.pth'
+    model_dir = 'u2net/saved_models/u2netp/u2netp.pth'
 
     if(model_name=='u2net'):
         print("...load U2NET---173.6 MB")
@@ -15,7 +15,8 @@ def model(model_name='u2net'):
     elif(model_name=='u2netp'):
         print("...load U2NEP---4.7 MB")
         net = U2NETP(3,1)
-    net.load_state_dict(torch.load(model_dir))
+    net.load_state_dict(torch.load(model_dir, map_location=torch.device('cpu')))
+
 
     if torch.cuda.is_available():
         net.cuda()
